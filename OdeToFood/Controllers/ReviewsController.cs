896c9e66460c93113ce890db2132f059ebf6cf26
bnoel0703 <bnoel0703@gmail.com>
2019-04-12 17:42:20 -0400

    Module 4 - Code Blocks

---
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        // GET: Review
+        public ActionResult Index()
+        {
+            var model = 
+                from r in _reviews
+                orderby r.Country
+                select r;
+            return View(model);
+        }
+
+        // GET: Review/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Review/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Review/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Review/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Review/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Review/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Review/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        static List<RestaurantReview> _reviews = new List<RestaurantReview>()
+        {
+            new RestaurantReview
+            {
+                Id = 1,
+                Name = "Cinnamon Club",
+                City = "London",
+                Country = "UK",
+                Rating = 10
+            },
+            new RestaurantReview
+            {
+                Id = 2,
+                Name = "Marrakesh",
+                City = "D.C",
+                Country = "USA",
+                Rating = 10
+            },
+            new RestaurantReview
+            {
+                Id = 3,
+                Name = "The House of Elliot",
+                City = "Ghent",
+                Country = "Belgium",
+                Rating = 10
+            }
+        };
+    }
+}
