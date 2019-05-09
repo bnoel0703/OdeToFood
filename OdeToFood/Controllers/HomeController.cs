45149c789fbf755cd6c813c310558055e2651493
bnoel0703 <bnoel0703@gmail.com>
2019-05-09 11:57:03 -0400

    Added Entity Framework

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -9,13 +9,13 @@ namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        OdeToFoodDb _db = new OdeToFoodDb();
+
         public ActionResult Index()
         {
-            var message = "Hello!";
+            var model = _db.Restaurants.ToList();
 
-            ViewBag.Message = message;
-
-            return View();
+            return View(model);
         }
 
         public ActionResult About()
@@ -34,6 +34,16 @@ namespace OdeToFood.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+
+            base.Dispose(disposing);
         }
     }
 }
--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -1,132 +1,132 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using OdeToFood.Models;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+//using OdeToFood.Models;
 
-namespace OdeToFood.Controllers
-{
-    public class ReviewsController : Controller
-    {
-        [ChildActionOnly]
-        public ActionResult BestReview()
-        {
-            var bestReview = from r in _reviews
-                             orderby r.Rating descending
-                             select r;
+//namespace OdeToFood.Controllers
+//{
+//    public class ReviewsController : Controller
+//    {
+//        [ChildActionOnly]
+//        public ActionResult BestReview()
+//        {
+//            var bestReview = from r in _reviews
+//                             orderby r.Rating descending
+//                             select r;
 
-            return PartialView("_Review", bestReview.First());
-        }
+//            return PartialView("_Review", bestReview.First());
+//        }
 
-        // GET: Review
-        public ActionResult Index()
-        {
-            var model = 
-                from r in _reviews
-                orderby r.Country
-                select r;
-            return View(model);
-        }
+//        // GET: Review
+//        public ActionResult Index()
+//        {
+//            var model = 
+//                from r in _reviews
+//                orderby r.Country
+//                select r;
+//            return View(model);
+//        }
 
-        // GET: Review/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+//        // GET: Review/Details/5
+//        public ActionResult Details(int id)
+//        {
+//            return View();
+//        }
 
-        // GET: Review/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+//        // GET: Review/Create
+//        public ActionResult Create()
+//        {
+//            return View();
+//        }
 
-        // POST: Review/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+//        // POST: Review/Create
+//        [HttpPost]
+//        public ActionResult Create(FormCollection collection)
+//        {
+//            try
+//            {
+//                // TODO: Add insert logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+//                return RedirectToAction("Index");
+//            }
+//            catch
+//            {
+//                return View();
+//            }
+//        }
 
-        // GET: Review/Edit/5
-        public ActionResult Edit(int id)
-        {
-            var review = _reviews.Single(r => r.Id == id);
+//        // GET: Review/Edit/5
+//        public ActionResult Edit(int id)
+//        {
+//            var review = _reviews.Single(r => r.Id == id);
 
-            return View(review);
-        }
+//            return View(review);
+//        }
 
-        // POST: Review/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            var review = _reviews.Single(r => r.Id == id);
-            if (TryUpdateModel(review))
-            {
-                //..
-                return RedirectToAction("Index");
-            }
+//        // POST: Review/Edit/5
+//        [HttpPost]
+//        public ActionResult Edit(int id, FormCollection collection)
+//        {
+//            var review = _reviews.Single(r => r.Id == id);
+//            if (TryUpdateModel(review))
+//            {
+//                //..
+//                return RedirectToAction("Index");
+//            }
 
-            return View(review);
-        }
+//            return View(review);
+//        }
 
-        // GET: Review/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+//        // GET: Review/Delete/5
+//        public ActionResult Delete(int id)
+//        {
+//            return View();
+//        }
 
-        // POST: Review/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+//        // POST: Review/Delete/5
+//        [HttpPost]
+//        public ActionResult Delete(int id, FormCollection collection)
+//        {
+//            try
+//            {
+//                // TODO: Add delete logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+//                return RedirectToAction("Index");
+//            }
+//            catch
+//            {
+//                return View();
+//            }
+//        }
 
-        static List<RestaurantReview> _reviews = new List<RestaurantReview>()
-        {
-            new RestaurantReview
-            {
-                Id = 1,
-                Name = "Cinnamon Club",
-                City = "London",
-                Country = "UK",
-                Rating = 10
-            },
-            new RestaurantReview
-            {
-                Id = 2,
-                Name = "Marrakesh",
-                City = "D.C",
-                Country = "USA",
-                Rating = 10
-            },
-            new RestaurantReview
-            {
-                Id = 3,
-                Name = "The House of Elliot",
-                City = "Ghent",
-                Country = "Belgium",
-                Rating = 10
-            }
-        };
-    }
-}
+//        static List<RestaurantReview> _reviews = new List<RestaurantReview>()
+//        {
+//            new RestaurantReview
+//            {
+//                Id = 1,
+//                Name = "Cinnamon Club",
+//                City = "London",
+//                Country = "UK",
+//                Rating = 10
+//            },
+//            new RestaurantReview
+//            {
+//                Id = 2,
+//                Name = "Marrakesh",
+//                City = "D.C",
+//                Country = "USA",
+//                Rating = 10
+//            },
+//            new RestaurantReview
+//            {
+//                Id = 3,
+//                Name = "The House of Elliot",
+//                City = "Ghent",
+//                Country = "Belgium",
+//                Rating = 10
+//            }
+//        };
+//    }
+//}
