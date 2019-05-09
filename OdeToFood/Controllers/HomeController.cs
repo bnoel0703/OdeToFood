39a8de301edd427b3de341e16fcefce1b398ebd8
bnoel0703 <bnoel0703@gmail.com>
2019-05-09 14:00:17 -0400

    Used LINQ

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -13,7 +13,28 @@ namespace OdeToFood.Controllers
 
         public ActionResult Index()
         {
-            var model = _db.Restaurants.ToList();
+            //var model =
+            //    from r in _db.Restaurants
+            //    orderby r.Reviews.Average(review => review.Rating) descending
+            //    select new RestaurantListViewModel
+            //    {
+            //        Id = r.Id,
+            //        Name = r.Name,
+            //        City = r.City,
+            //        Country = r.Country,
+            //        CountOfReviews = r.Reviews.Count()
+            //    };
+
+            var model =
+                _db.Restaurants.OrderByDescending(r => r.Reviews.Average(review => review.Rating)).Select(r =>
+                    new RestaurantListViewModel
+                    {
+                        Id = r.Id,
+                        Name = r.Name,
+                        City = r.City,
+                        Country = r.Country,
+                        CountOfReviews = r.Reviews.Count()
+                    });
 
             return View(model);
         }
