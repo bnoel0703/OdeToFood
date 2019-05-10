5c9f1c4ced6f2839b0dc97d0f90729e380f6b5e5
bnoel0703 <bnoel0703@gmail.com>
2019-05-10 13:57:10 -0400

    Added views for Reviews

--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -1,132 +1,31 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using OdeToFood.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
 
-//namespace OdeToFood.Controllers
-//{
-//    public class ReviewsController : Controller
-//    {
-//        [ChildActionOnly]
-//        public ActionResult BestReview()
-//        {
-//            var bestReview = from r in _reviews
-//                             orderby r.Rating descending
-//                             select r;
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly OdeToFoodDb _db = new OdeToFoodDb();
 
-//            return PartialView("_Review", bestReview.First());
-//        }
+        // GET: Review
+        public ActionResult Index([Bind(Prefix="id")]int restaurantId)
+        {
+            var restaurant = _db.Restaurants.Find(restaurantId);
+            if (restaurant != null)
+                return View(restaurant);
+            return HttpNotFound();
+        }
 
-//        // GET: Review
-//        public ActionResult Index()
-//        {
-//            var model = 
-//                from r in _reviews
-//                orderby r.Country
-//                select r;
-//            return View(model);
-//        }
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
 
-//        // GET: Review/Details/5
-//        public ActionResult Details(int id)
-//        {
-//            return View();
-//        }
 
-//        // GET: Review/Create
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
-
-//        // POST: Review/Create
-//        [HttpPost]
-//        public ActionResult Create(FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add insert logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        // GET: Review/Edit/5
-//        public ActionResult Edit(int id)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-
-//            return View(review);
-//        }
-
-//        // POST: Review/Edit/5
-//        [HttpPost]
-//        public ActionResult Edit(int id, FormCollection collection)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-//            if (TryUpdateModel(review))
-//            {
-//                //..
-//                return RedirectToAction("Index");
-//            }
-
-//            return View(review);
-//        }
-
-//        // GET: Review/Delete/5
-//        public ActionResult Delete(int id)
-//        {
-//            return View();
-//        }
-
-//        // POST: Review/Delete/5
-//        [HttpPost]
-//        public ActionResult Delete(int id, FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add delete logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        static List<RestaurantReview> _reviews = new List<RestaurantReview>()
-//        {
-//            new RestaurantReview
-//            {
-//                Id = 1,
-//                Name = "Cinnamon Club",
-//                City = "London",
-//                Country = "UK",
-//                Rating = 10
-//            },
-//            new RestaurantReview
-//            {
-//                Id = 2,
-//                Name = "Marrakesh",
-//                City = "D.C",
-//                Country = "USA",
-//                Rating = 10
-//            },
-//            new RestaurantReview
-//            {
-//                Id = 3,
-//                Name = "The House of Elliot",
-//                City = "Ghent",
-//                Country = "Belgium",
-//                Rating = 10
-//            }
-//        };
-//    }
-//}
+    }
+}
